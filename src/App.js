fb29965d4d03e1ed3af5c59a9c3085102e69bf8e
WonilKim <nonelove2@gmail.com>
2023-04-28 18:15:45 +0900

    230428 react

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import MyClock from './02/MyClock';
 import Box from './03/Box';
 import MyDiv from './04/MyDiv'; // 새로 만든 파일(함수) 이름. 첫글자는 대문자
 import Frcst from './05/Frcst';
+import Traffic from './06/Traffic'
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
       <Route path='/Box' element={<Box />} />
       <Route path='/mydiv' element={<MyDiv />} />
       <Route path='/Frcst' element={<Frcst />} />
+      <Route path='/Traffic' element={<Traffic />} />
     </Routes>
   );
 }
--- src/Introduce.js
+++ src/Introduce.js
@@ -24,7 +24,8 @@ const Introduce = () => {
                                 <img className={style.react_image} src={react_image} alt="" />
                             </div>
                             <div className={style.study_list}>
-                                <ol> 
+                                <ol>
+                                    <li><Link to="/Traffic">[05.02] React traffic accident</Link></li>
                                     <li><Link to="/Frcst">[04.27] React fine dust forecast</Link></li>
                                     <li><Link to="/mydiv">[04.25] React custom component</Link></li>
                                     <li><Link to="/Box">[04.25] React box office</Link></li>
