f702b23d60a13e3ed148bc51dd1b8391ab537f2d
WonilKim <nonelove2@gmail.com>
2023-05-08 18:02:56 +0900

    230508 fashion

--- src/fashion/Fashion.js
+++ src/fashion/Fashion.js
@@ -12,14 +12,20 @@ const Fashion = () => {
         '밀리터리', '소피스트케이티드', '모던', '오리엔탈', '웨스턴', '젠더리스', '컨트리', '아방가르드'].sort();
 
     // 카테고리
-    const outer_category = ['가디건', '짚업', '베스트', '코트', '재킷', '점퍼', '패딩'];
-    const top_category = ['브라탑', '후드티', '탑', '티셔츠', '셔츠', '블라우스', '니트웨어'];
-    const bottom_category = ['스커트', '청바지', '조거팬츠', '팬츠', '래깅스'];
-    const onepiece_category = ['드레스', '점프수트'];
+    const outer_category = ['가디건', '짚업', '베스트', '코트', '재킷', '점퍼', '패딩'].sort();
+    const top_category = ['브라탑', '후드티', '탑', '티셔츠', '셔츠', '블라우스', '니트웨어'].sort();
+    const bottom_category = ['스커트', '청바지', '조거팬츠', '팬츠', '래깅스'].sort();
+    const onepiece_category = ['드레스', '점프수트'].sort();
+
+    // 핏
+    const outer_fit = ['노멀', '루즈', '오버사이즈', '타이트'];
+    const top_fit = outer_fit;
+    const bottom_fit = ['노멀', '루즈', '스키니', '와이드', '벨보텀'];
+    const onepiece_fit = outer_fit;
 
     // 기장
-    const outer_length = ['롱', '맥시', '하프', '노말', '크롭'];
-    const top_length = ['롱', '노멀', '크롭'];
+    const outer_length = ['노말', '롱', '크롭', '맥시', '하프'];
+    const top_length = ['노멀', '롱', '크롭'];
     const bottom_length = ['맥시', '니렝스', '미니', '미디', '발목'];
     const onepiece_length = bottom_length;
 
@@ -29,18 +35,53 @@ const Fashion = () => {
     const bottom_sleeve_length = [];
     const onepiece_sleeve_length = top_sleeve_length;
 
-    // neckline
-    const outer_neckline = ['오프숄더', '원숄더', '보트넥', '후드', '터틀넥', '브이넥', '스퀘어넥', '라운드넥', '유넥', '스위트하트', '노카라', '홀터넥'];
+    // 색상
+    const common_color = ['블루', '민트', '골드', '레드', '옐로우', '브라운', '그린', '퍼플', '라벤더',
+        '네이비', '네온', '실버', '화이트', '와인', '카키', '그레이', '스카이블루', '블랙', '베이지', '오렌지',
+        '핑크'].sort();
+
+    // 재질
+    const common_material = ['니트', '시폰', '린넨', '패딩', '퍼', '스판덱스', '실크', '코듀로이',
+        '시퀸/글리터', '저지', '우븐', '가죽', '메시', '네오프렌', '자카드', '헤어 니트', '플리스', '무스탕',
+        '울/캐시미어', '데님', '스웨이드', '트위드', '비닐/PVC', '벨벳', '레이스'].sort();
+
+    // 프린트
+    const common_print = ['하운즈투스', '믹스', '카무플라쥬', '페이즐리', '지그재그', '도트', '호피',
+        '플로럴', '그래픽', '타이다이', '하트', '무지', '깅엄', '아가일', '체크', '스트라이프', '해골',
+        '뱀피', '그라데이션', '지브라', '레터링'].sort();
+
+    // 디테일
+    const common_detail = ['싱글브레스티드', '비즈', '체인', '프린지', '버클', '디스트로이드', '패치워크',
+        '러플', '페플럼', '프릴', '글리터', '지퍼', '드롭숄더', '레이스업', '스티치', '퀄팅', '드롭웨이스트',
+        '플레어', '비대칭', '태슬', '슬릿', '퍼프', '스팽글', '퍼트리밍', '스터드', '스트링', '단추', '폼폼',
+        'X스트랩', '롤업', '니트꽈베기', '컷아웃', '리본', '더블브레스티드', '자수', '포켓', '플리츠', '띠',
+        '레이스', '셔링'].sort();
+
+    // 넥라인
+    const outer_neckline = ['오프숄더', '원숄더', '보트넥', '후드', '터틀넥', '브이넥', '스퀘어넥',
+        '라운드넥', '유넥', '스위트하트', '노카라', '홀터넥'].sort();
     const top_neckline = outer_neckline;
     const bottom_neckline = [];
     const onepiece_neckline = outer_neckline;
 
-    // fit
-    const outer_fit = ['오버사이즈', '노멀', '타이트', '루즈'];
-    const top_fit = outer_fit;
-    const bottom_fit = ['노멀', '스키니', '와이드', '루즈', '벨보텀'];
-    const onepiece_fit = outer_fit;
+    // 서브 스타일
+    const common_sub_styles = common_styles;
 
+    // 서브 컬러
+    const common_sub_color = common_color;
+
+    // 카라
+    const outer_collar = ['피터팬칼라', '셔츠칼라', '보우칼라', '폴로칼라', '세일러칼라', '테일러드칼라', '밴드칼라', '차이나칼라', '숄칼라'].sort();
+    const top_collar = outer_collar;
+    const bottom_collar = [];
+    const onepiece_collar = outer_collar;
+
+    const [styleTags, setStyleTags] = useState(0);
+    const [selectedStyles, setSelectedStyles] = useState([]);
+    useEffect(() => {
+        console.log('-- useEffect() selectedStyles changed', selectedStyles);
+
+    }, [selectedStyles]);
 
     const [categoryTags, setCategoryTags] = useState(0);
     const [selectedCategories, setSelectedCategories] = useState([]);
@@ -48,6 +89,13 @@ const Fashion = () => {
         console.log('-- useEffect() selectedCategories changed', selectedCategories);
 
     }, [selectedCategories]);
+
+    const [fitTags, setFitTags] = useState(0);
+    const [selectedFits, setSelectedFits] = useState([]);
+    useEffect(() => {
+        console.log('-- useEffect() selectedFits changed', selectedFits);
+
+    }, [selectedFits]);
 
     const [lengthTags, setLengthTags] = useState(0);
     const [selectedLengths, setSelectedLengths] = useState([]);
@@ -63,6 +111,34 @@ const Fashion = () => {
 
     }, [selectedSleeveLengths]);
 
+    const [colorTags, setColorTags] = useState(0);
+    const [selectedColors, setSelectedColors] = useState([]);
+    useEffect(() => {
+        console.log('-- useEffect() selectedColors changed', selectedColors);
+
+    }, [selectedColors]);
+
+    const [materialTags, setMaterialTags] = useState(0);
+    const [selectedMaterials, setSelectedMaterials] = useState([]);
+    useEffect(() => {
+        console.log('-- useEffect() selectedMaterials changed', selectedMaterials);
+
+    }, [selectedMaterials]);
+
+    const [detailTags, setDetailTags] = useState(0);
+    const [selectedDetails, setSelectedDetails] = useState([]);
+    useEffect(() => {
+        console.log('-- useEffect() selectedDetails changed', selectedDetails);
+
+    }, [selectedDetails]);
+
+    const [printTags, setPrintTags] = useState(0);
+    const [selectedPrints, setSelectedPrints] = useState([]);
+    useEffect(() => {
+        console.log('-- useEffect() selectedPrints changed', selectedPrints);
+
+    }, [selectedPrints]);
+
     const [necklineTags, setNecklineTags] = useState(0);
     const [selectedNecklines, setSelectedNecklines] = useState([]);
     useEffect(() => {
@@ -70,19 +146,12 @@ const Fashion = () => {
 
     }, [selectedNecklines]);
 
-    const [fitTags, setFitTags] = useState(0);
-    const [selectedFits, setSelectedFits] = useState([]);
+    const [collarTags, setCollarTags] = useState(0);
+    const [selectedCollars, setSelectedCollars] = useState([]);
     useEffect(() => {
-        console.log('-- useEffect() selectedFits changed', selectedFits);
+        console.log('-- useEffect() selectedCollars changed', selectedCollars);
 
-    }, [selectedFits]);
-
-    const [styleTags, setStyleTags] = useState(0);
-    const [selectedStyles, setSelectedStyles] = useState([]);
-    useEffect(() => {
-        console.log('-- useEffect() selectedStyles changed', selectedStyles);
-
-    }, [selectedStyles]);
+    }, [selectedCollars]);
 
 
     //    const styleTags = (
@@ -104,12 +173,17 @@ const Fashion = () => {
     useEffect(() => {
         console.log('-- useEffect() selectedPart changed', selectedPart);
 
+        let styles = [];
         let categories = [];
+        let fits = [];
         let lengths = [];
         let sleeve_lengths = [];
+        let colors = [];
+        let materials = [];
+        let details = [];
+        let prints = [];
         let necklines = [];
-        let fits = [];
-        let styles = [];
+        let collars = [];
 
         switch (selectedPart) {
             case "상의":
@@ -118,7 +192,14 @@ const Fashion = () => {
                 sleeve_lengths = top_sleeve_length;
                 necklines = top_neckline;
                 fits = top_fit;
+                collars = top_collar;
+
                 styles = common_styles;
+                colors = common_color;
+                materials = common_material;
+                details = common_detail;
+                prints = common_print;
+
                 break;
             case "하의":
                 categories = [...bottom_category].sort();
@@ -126,7 +207,13 @@ const Fashion = () => {
                 sleeve_lengths = bottom_sleeve_length;
                 necklines = bottom_neckline;
                 fits = bottom_fit;
+                collars = bottom_collar;
+
                 styles = common_styles;
+                colors = common_color;
+                materials = common_material;
+                details = common_detail;
+                prints = common_print;
                 break;
             case "아우터":
                 categories = [...outer_category].sort();
@@ -134,7 +221,13 @@ const Fashion = () => {
                 sleeve_lengths = outer_sleeve_length;
                 necklines = outer_neckline;
                 fits = outer_fit;
+                collars = outer_collar;
+
                 styles = common_styles;
+                colors = common_color;
+                materials = common_material;
+                details = common_detail;
+                prints = common_print;
                 break;
             case "원피스":
                 categories = [...onepiece_category].sort();
@@ -142,17 +235,45 @@ const Fashion = () => {
                 sleeve_lengths = onepiece_sleeve_length;
                 necklines = onepiece_neckline;
                 fits = onepiece_fit;
+                collars = onepiece_collar;
+
                 styles = common_styles;
+                colors = common_color;
+                materials = common_material;
+                details = common_detail;
+                prints = common_print;
                 break;
             default:
                 break;
         }
+
+        setStyleTags(
+            styles.map((item) => {
+                return (
+                    <label key={item} >
+                        <input type="checkbox" className="styleCheckbox" id={item} name={item} value={item} onChange={onStyleChanged} />
+                        {item}
+                    </label>
+                );
+            })
+        );
 
         setCategoryTags(
             categories.map((item) => {
                 return (
                     <label key={item} >
                         <input type="checkbox" className="categoryCheckbox" id={item} name={item} value={item} onChange={onCategoryChanged} />
+                        {item}
+                    </label>
+                );
+            })
+        );
+
+        setFitTags(
+            fits.map((item) => {
+                return (
+                    <label key={item} >
+                        <input type="checkbox" className="fitCheckbox" id={item} name={item} value={item} onChange={onFitChanged} />
                         {item}
                     </label>
                 );
@@ -181,6 +302,61 @@ const Fashion = () => {
             })
         );
 
+        setColorTags(
+            colors.map((item) => {
+                return (
+                    <label key={item} >
+                        <input type="checkbox" className="colorCheckbox" id={item} name={item} value={item} onChange={onNecklineChanged} />
+                        {item}
+                    </label>
+                );
+            })
+        );
+
+        setMaterialTags(
+            materials.map((item) => {
+                return (
+                    <label key={item} >
+                        <input type="checkbox" className="materialCheckbox" id={item} name={item} value={item} onChange={onMaterialChanged} />
+                        {item}
+                    </label>
+                );
+            })
+        );
+
+        setDetailTags(
+            details.map((item) => {
+                return (
+                    <label key={item} >
+                        <input type="checkbox" className="detailCheckbox" id={item} name={item} value={item} onChange={onDetailChanged} />
+                        {item}
+                    </label>
+                );
+            })
+        );
+
+        setPrintTags(
+            prints.map((item) => {
+                return (
+                    <label key={item} >
+                        <input type="checkbox" className="printCheckbox" id={item} name={item} value={item} onChange={onPrintChanged} />
+                        {item}
+                    </label>
+                );
+            })
+        );
+
+        setCollarTags(
+            collars.map((item) => {
+                return (
+                    <label key={item} >
+                        <input type="checkbox" className="collarCheckbox" id={item} name={item} value={item} onChange={onCollarChanged} />
+                        {item}
+                    </label>
+                );
+            })
+        );
+
         setNecklineTags(
             necklines.map((item) => {
                 return (
@@ -192,31 +368,24 @@ const Fashion = () => {
             })
         );
 
-        setFitTags(
-            fits.map((item) => {
-                return (
-                    <label key={item} >
-                        <input type="checkbox" className="fitCheckbox" id={item} name={item} value={item} onChange={onFitChanged} />
-                        {item}
-                    </label>
-                );
-            })
-        );
 
-        setStyleTags(
-            styles.map((item) => {
-                return (
-                    <label key={item} >
-                        <input type="checkbox" className="styleCheckbox" id={item} name={item} value={item} onChange={onStyleChanged} />
-                        {item}
-                    </label>
-                );
-            })
-        );
 
 
     }, [selectedPart]);
 
+    const onStyleChanged = (e) => {
+        console.log("onStyleChanged()");
+        // console.log(e.target);
+
+        let values = []
+        let checkedItems = document.querySelectorAll('.styleCheckbox:checked');
+        // console.log("checkedItems = ", checkedItems);
+        for (let item of checkedItems) {
+            // console.log(item.value);
+            values.push(item.value);
+        }
+        setSelectedStyles(values);
+    }
 
     const onCategoryChanged = (e) => {
         console.log("onCategoryChanged()");
@@ -230,6 +399,20 @@ const Fashion = () => {
             values.push(item.value);
         }
         setSelectedCategories(values);
+    }
+
+    const onFitChanged = (e) => {
+        console.log("onFitChanged()");
+        // console.log(e.target);
+
+        let values = []
+        let checkedItems = document.querySelectorAll('.fitCheckbox:checked');
+        // console.log("checkedItems = ", checkedItems);
+        for (let item of checkedItems) {
+            // console.log(item.value);
+            values.push(item.value);
+        }
+        setSelectedFits(values);
     }
 
     const onLengthChanged = (e) => {
@@ -260,6 +443,64 @@ const Fashion = () => {
         setSelectedSleeveLengths(values);
     }
 
+
+
+    const onColorChanged = (e) => {
+        console.log("onColorChanged()");
+        // console.log(e.target);
+
+        let values = []
+        let checkedItems = document.querySelectorAll('.colorCheckbox:checked');
+        // console.log("checkedItems = ", checkedItems);
+        for (let item of checkedItems) {
+            // console.log(item.value);
+            values.push(item.value);
+        }
+        setSelectedColors(values);
+    }
+
+    const onMaterialChanged = (e) => {
+        console.log("onMaterialChanged()");
+        // console.log(e.target);
+
+        let values = []
+        let checkedItems = document.querySelectorAll('.materialCheckbox:checked');
+        // console.log("checkedItems = ", checkedItems);
+        for (let item of checkedItems) {
+            // console.log(item.value);
+            values.push(item.value);
+        }
+        setSelectedMaterials(values);
+    }
+
+    const onDetailChanged = (e) => {
+        console.log("onDetailChanged()");
+        // console.log(e.target);
+
+        let values = []
+        let checkedItems = document.querySelectorAll('.detailCheckbox:checked');
+        // console.log("checkedItems = ", checkedItems);
+        for (let item of checkedItems) {
+            // console.log(item.value);
+            values.push(item.value);
+        }
+        setSelectedDetails(values);
+    }
+
+    const onPrintChanged = (e) => {
+        console.log("onPrintChanged()");
+        // console.log(e.target);
+
+        let values = []
+        let checkedItems = document.querySelectorAll('.printCheckbox:checked');
+        // console.log("checkedItems = ", checkedItems);
+        for (let item of checkedItems) {
+            // console.log(item.value);
+            values.push(item.value);
+        }
+        setSelectedPrints(values);
+    }
+
     const onNecklineChanged = (e) => {
         console.log("onNecklineChanged()");
         // console.log(e.target);
@@ -274,34 +515,19 @@ const Fashion = () => {
         setSelectedNecklines(values);
     }
 
-    const onFitChanged = (e) => {
-        console.log("onFitChanged()");
+    const onCollarChanged = (e) => {
+        console.log("onCollarChanged()");
         // console.log(e.target);
 
         let values = []
-        let checkedItems = document.querySelectorAll('.fitCheckbox:checked');
+        let checkedItems = document.querySelectorAll('.collarCheckbox:checked');
         // console.log("checkedItems = ", checkedItems);
         for (let item of checkedItems) {
             // console.log(item.value);
             values.push(item.value);
         }
-        setSelectedFits(values);
+        setSelectedCollars(values);
     }
-
-    const onStyleChanged = (e) => {
-        console.log("onStyleChanged()");
-        // console.log(e.target);
-
-        let values = []
-        let checkedItems = document.querySelectorAll('.styleCheckbox:checked');
-        // console.log("checkedItems = ", checkedItems);
-        for (let item of checkedItems) {
-            // console.log(item.value);
-            values.push(item.value);
-        }
-        setSelectedStyles(values);
-    }
-
 
 
     // 의상 종류 라디오버튼 선택 변경 이벤트 함수
@@ -356,23 +582,40 @@ const Fashion = () => {
                     </div>
                 </details>
 
-                <FashionChecker summary="카테고리" description="선호하는 카테고리를 모두 선택하세요."
+                <FashionChecker summary="스타일" description=""
+                    tags={styleTags} setTags={setStyleTags} />
+
+                <FashionChecker summary="카테고리" description=""
                     tags={categoryTags} setTags={setCategoryTags} />
 
-                <FashionChecker summary="기장" description="원하는 기장을 모두 선택하세요."
-                    tags={lengthTags} setTags={setLengthTags} />
-
-                <FashionChecker summary="소매 기장" description="원하는 소매 기장을 모두 선택하세요."
-                    tags={sleeveLengthTags} setTags={setSleeveLengthTags} />
-
-                <FashionChecker summary="넥 라인" description="원하는 넥 라인을 모두 선택하세요."
-                    tags={necklineTags} setTags={setNecklineTags} />
-
-                <FashionChecker summary="핏" description="원하는 핏을 모두 선택하세요."
+                <FashionChecker summary="핏" description=""
                     tags={fitTags} setTags={setFitTags} />
 
-                <FashionChecker summary="스타일" description="원하는 스타일을 모두 선택하세요."
-                    tags={styleTags} setTags={setStyleTags} />
+                <FashionChecker summary="기장" description=""
+                    tags={lengthTags} setTags={setLengthTags} />
+
+                <FashionChecker summary="소매 기장" description=""
+                    tags={sleeveLengthTags} setTags={setSleeveLengthTags} />
+
+                <FashionChecker summary="색상" description=""
+                    tags={colorTags} setTags={setColorTags} />
+
+                <FashionChecker summary="재질" description=""
+                    tags={materialTags} setTags={setMaterialTags} />
+
+                <FashionChecker summary="디테일" description=""
+                    tags={detailTags} setTags={setDetailTags} />
+
+                <FashionChecker summary="프린트" description=""
+                    tags={printTags} setTags={setPrintTags} />
+
+                <FashionChecker summary="넥 라인" description=""
+                    tags={necklineTags} setTags={setNecklineTags} />
+
+                <FashionChecker summary="카라" description=""
+                    tags={collarTags} setTags={setCollarTags} />
+
+
 
 
             </article>
